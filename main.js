92a7aaa7e929cec5e506aa6903813be9b858b32c
amrfatouh <66204477+amrfatouh@users.noreply.github.com>
2020-09-02 04:59:33 +0200

    better design

--- main.js
+++ main.js
@@ -4,67 +4,65 @@ let prevBtn = document.querySelector('.prev');
 let nextBtn = document.querySelector('.next');
 let paginationItems = document.querySelectorAll('.slides-control ul li');
 
+let currentElement = 0;
+
+//randomizing the starting slide each time the page loads
+(function startView() {
+    currentElement = Math.floor(Math.random() * 5);/* get nums from 0 to 4 */
+    updateView();
+}());
+
+//update the view depending on the value of 'currentElement'
+function updateView() {
+    removeActive(sliderImages, paginationItems);
+    addActive(sliderImages[currentElement], paginationItems[currentElement]);
+    imageNumber.textContent = `slide ${currentElement + 1} of ${sliderImages.length}`;
+    checkAndDisableBtns();
+}
+
 //adding previous and next buttons functionality
 prevBtn.onclick = function () {
-    let activeIndex = determineActiveIndex(sliderImages);
-    if (activeIndex > 0) {
-        sliderImages[activeIndex].classList.remove('active');
-        sliderImages[activeIndex - 1].classList.add('active');
-        paginationItems[activeIndex].classList.remove('active');
-        paginationItems[activeIndex - 1].classList.add('active');
-        imageNumber.textContent = `slide ${activeIndex} of ${sliderImages.length}`;
+    if (currentElement > 0) {
+        currentElement--;
+        updateView();
     }
-    checkAndDisableBtns();
-
 }
 nextBtn.onclick = function () {
-    let activeIndex = determineActiveIndex(sliderImages);
-    if (activeIndex < sliderImages.length - 1) {
-        sliderImages[activeIndex].classList.remove('active');
-        sliderImages[activeIndex + 1].classList.add('active');
-        paginationItems[activeIndex].classList.remove('active');
-        paginationItems[activeIndex + 1].classList.add('active');
-        imageNumber.textContent = `slide ${activeIndex + 2} of ${sliderImages.length}`;
+    if (currentElement < sliderImages.length - 1) {
+        currentElement++;
+        updateView();
     }
-    checkAndDisableBtns();
 
 }
 
 //adding clicking on pagination items functionality
 paginationItems.forEach((item, index) => {
     item.addEventListener('click', function () {
-        sliderImages.forEach(img => img.classList.remove('active'));
-        sliderImages[index].classList.add('active');
-        paginationItems.forEach(i => i.classList.remove('active'));
-        item.classList.add('active');
-        imageNumber.textContent = `slide ${index + 1} of ${sliderImages.length}`;
-        checkAndDisableBtns();
+        currentElement = index;
+        updateView();
     })
 })
 
 //function that checks if the slider arrived at one of its 2 ends and do what should be done
 function checkAndDisableBtns() {
-    let activeIndex = determineActiveIndex(sliderImages);
-    console.log(activeIndex);
-    if (activeIndex <= 0) {
+    if (currentElement <= 0) {
         prevBtn.setAttribute('disabled', 'true');
     } else {
         prevBtn.removeAttribute('disabled');
     }
-    if (activeIndex >= sliderImages.length - 1) {
+    if (currentElement >= sliderImages.length - 1) {
         nextBtn.setAttribute('disabled', 'true');
     } else {
         nextBtn.removeAttribute('disabled');
     }
 }
 
-//function that determines the index of the active element in any list you give
-let determineActiveIndex = (list) => {
-    let activeIndex;
-    list.forEach((item, Index) => {
-        if (item.classList.contains('active'))
-            activeIndex = Index;
-    })
-    return activeIndex;
+//function that removes 'active' class from any list
+function removeActive(...lists) {
+    lists.forEach(list => list.forEach(item => item.classList.remove('active')));
 }
 
+//function that adds 'active' class to any item
+function addActive(...items) {
+    items.forEach(item => item.classList.add('active'));
+}
